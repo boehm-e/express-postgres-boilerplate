c0e1821f63a80e29c09b1c052f202a9174671b22
Liazidi Amine <liazidiiamine@gmail.com>
2017-06-19 11:40:46 +0200

    rectification2

--- API/index.js
+++ API/index.js
@@ -1,11 +1,10 @@
 import express		from 'express';
-import userRoutes 	from './routes/user';
-import usersRoutes 	from './routes/users';
+import {router1, router2} from './routes/user';
 const router = express.Router();
 
 router.get('/health-check', (_, res) => res.send('OK'));
 
-router.use('/users', usersRoutes);
-router.use('/user', userRoutes);
+router.use('/user', router1);
+router.use('/users', router2);
 
 export default router;
